64270197e4faf70c4a2e0561a290a3ed072fc5ca
hp <hp@DESKTOP-75BFR6A>
2023-08-21 22:52:35 +0530

    Add project files.

---
+++ labfirst4.fourth/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labfirst4.fourth
+{
+    class Fourth
+    {
+        static void Main(string[] args)
+        {
+            int[] Arr = new int[5];
+            Console.WriteLine("Enter the element of array : ");
+            for (int i = 0; i < 5; i++) Arr[i] = Convert.ToInt32(Console.ReadLine());
+            int oddcount = 0, evencount = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                if (Arr[i] % 2 == 0)
+                    evencount++;
+                else
+                {
+                    oddcount++;
+                }
+            }
+            Console.WriteLine("Odd elements in array : " + oddcount);
+            Console.WriteLine("Even elements in array : " + evencount);
+            Console.ReadLine();
+        }
+    }
+}
